2bc2b6417a9ce633caf8c283c1cd9934a6d4b6b8
vinecave <aromanov@wake-app.ru>
2021-12-22 21:49:12 +0300

    Modified extension class

--- DependencyInjection/VinecaveRegistryExtension.php
+++ DependencyInjection/VinecaveRegistryExtension.php
@@ -5,8 +5,9 @@ namespace Vinecave\Bundle\RegistryBundle\DependencyInjection;
 use Vinecave\Bundle\RegistryBundle\Registry\Registry;
 use Vinecave\Bundle\CollectionBundle\Container\AbstractExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class VinecaveRegistryExtension extends AbstractExtension
+class VinecaveRegistryExtension extends Extension
 {
     /**
      * {@inheritdoc}
@@ -16,7 +17,5 @@ class VinecaveRegistryExtension extends AbstractExtension
         $container
             ->registerForAutoconfiguration(Registry::class)
             ->addTag(Registry::class);
-
-        $this->loadExtension($container, __DIR__);
     }
 }
